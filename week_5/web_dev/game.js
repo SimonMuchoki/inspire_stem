174a51cc94f3baa7e0cc437684852b513acf2c13
SimonMuchoki <munenesimon04@gmail.com>
2024-03-21 08:21:47 +0300

    .

--- week_5/web_dev/game.js
+++ week_5/web_dev/game.js
@@ -1,1 +1,25 @@
-alert("welcome")
+const randomNumber=Math.floor(Math.random()*100) +1
+
+let attempt = 0
+
+function checkGuess(){
+    const guess = parseInt(document.getElementById("guessfield").value)
+    attempt++
+
+    if(isNaN(guess) || guess < 1 || guess > 100){
+        setMessage("Please Enter a valid number between 1 and 100")
+        return
+    }
+
+    if(guess === randomNumber){
+        setMessage("Damn! You Passed")
+    }else if(guess < randomNumber){
+        setMessage("The Number is below requirements")
+    }else if(guess > randomNumber) {
+        setMessage("The Number is above requirement")
+    }
+    console.log(randomNumber)
+}
+function setMessage(message){
+    document.getElementById("message").textContent = message
+}
---
+++ week_6/calc/calc.js
@@ -0,0 +1,32 @@
+let input = document.getElementById("InputBox");
+//console.log(input)
+let buttons = document.querySelectorAll("button"); // this is an array
+//console.log(buttons)
+
+let string="";
+
+
+let arr = Array.from(buttons)
+
+arr.forEach(button =>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == "="){
+            string = eval(string);
+            input.value = string;
+
+        }else if(e.target.innerHTML == "AC"){
+            string = "";
+            input.value = string;
+
+        }else if(e.target.innerHTML == "DEL"){
+            string = string.substring(0,string.length-1) ;
+            input.value = string;
+
+        }else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+
+
+    })
+})
